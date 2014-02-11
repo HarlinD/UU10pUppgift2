a0af358632fb8e7deecbb0c2cf067b3b94381845
HarlinD <daniel.harlin@gmail.com>
2014-02-11 11:46:47 +0100

    Initial Commit

---
+++ Uppgift2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Uppgift2
+//
+//  Created by Daniel Harlin on 2014-02-11.
+//  Copyright (c) 2014 Daniel Harlin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
